65b69666bd5e384bfb5e7cb1e9d0fc0c3e4962ec
Mgrga97 <marko.grg97@gmail.com>
2024-12-26 00:10:30 +0100

    Prvi commit samo Zimski zadaci

---
+++ CSHARP/ZimskiZadaci/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZimskiZadaci
+{
+    internal class Class1
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+
+}
---
+++ CSHARP/ZimskiZadaci/Program.cs
@@ -0,0 +1,7 @@
+﻿using ZimskiZadaci;
+
+Class1.Izvedi();
+
+// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
